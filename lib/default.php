edd2ca71079f0f6c0ff5d74da991730122f8eaa8
kabirweb <kabirweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 06:49:45 +0000

    Fix Errors
    git-svn-id: https://plugins.svn.wordpress.org/wp-top-5-social-sharing/trunk@1034287 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/default.php
@@ -0,0 +1,10 @@
+<?php
+$default = array(
+'wpt5ss_rotate' => 'on',
+'wpt5ss_zoom' => 'off',
+'wpt5ss_loc' => 'right',
+'wpt5ss_dev1' => '<a href="http://www.ahsanulkabir.com/" title="Web Design Bangladesh" class="cr_fix" rel="dofollow">Web Design Bangladesh</a>',
+'wpt5ss_dev2' => '<a href="http://www.a1netsolutions.com/" title="Web Design Bangladesh" class="cr_fix" rel="dofollow">Web Design Bangladesh</a>',
+'wpt5ss_dev3' => '<a href="http://www.ourmymensingh.com/" title="Mymensingh" class="cr_fix" rel="dofollow">Mymensingh</a>',
+);
+?>
